41dc9c72de24a8e412a005ea35e7030d73b333c5
mihye0924 <mihye0924@naver.com>
2023-12-07 14:47:04 +0900

    update MoveItem spliing

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,6 +1,6 @@
 import { Component } from "../core/heropy";
 
-export default class Moiveitem extends Component{
+export default class MoiveItem extends Component{
   constructor(props){
     super({
       props,
